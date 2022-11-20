a60f7ad0cd66fb626e54ed7371dcb13f28e39a43
Rickyflame <kimumuricky@gmail.com>
2022-11-20 13:33:08 +0300

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""start a flask web application
-the application listens on 0.0.0.0, port 5000
-"""
+'''Starts a Flask web application.Displays  “Hello HBNB!”'''
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
     '''home page display'''
-    return "HELLO HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
